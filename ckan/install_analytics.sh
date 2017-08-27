93f2d6970d931db923de2249ce43ff3fb3925fb1
Ori Hoch <ori@uumpa.com>
2017-08-27 23:05:06 +0300

    [WIP] trying to integrate google analytics extension

---
+++ ckan/install_analytics.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pip install --upgrade 'git+https://github.com/ckan/ckanext-googleanalytics.git@7c805cd7b2ee18ec6dfceadf1e04e3f024620db4#egg=ckanext-googleanalytics'
+pip install -r ckan/ckanext-googleanalytics-requirements.txt
