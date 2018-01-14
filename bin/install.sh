c157e7bb73d71b197122885b414ffd1d4299ed8b
Ori Hoch <ori@uumpa.com>
2018-01-14 15:33:17 +0200

    update to ckan 2.7.2 with fix for attachment of members to groups

--- bin/install.sh
+++ bin/install.sh
@@ -10,6 +10,6 @@ if [ -f "${1:-/tmp}/ckan-2.7.2/setup.py" ]; then
     echo "Trying to install ckan from local copy at ${1:-/tmp}/ckan-2.7.2/setup.py"
     pip install -e "${1:-/tmp}/ckan-2.7.2"
 else
-    pip install -e 'git+https://github.com/ckan/ckan.git@ckan-2.7.2#egg=ckan'
+    pip install -e 'git+https://github.com/OriHoch/ckan.git@ckan-2.7.2-fixing-odata-bug#egg=ckan'
 fi
 pip install -e ckan/ckanext-odata_org_il/
