4e1c73bb6718b94ababdb50b9d5d77bb4169f2c2
Ori Hoch <ori@uumpa.com>
2018-08-31 16:29:40 +0300

    fix docker entrypoint restart for gunicorn

--- ckan/entrypoint.sh
+++ ckan/entrypoint.sh
@@ -4,21 +4,16 @@
 
 if [ "${1} ${2}" == "server start" ]; then
     exec gunicorn --paste /etc/ckan/default/development.ini
-    # paster serve /etc/ckan/default/development.ini start &&\
-    # tail -f paster.log
-
-elif [ "${1} ${2}" == "server run" ]; then
-    exec paster serve /etc/ckan/default/development.ini
 
 elif [ "${1} ${2}" == "server restart" ]; then
-    paster serve /etc/ckan/default/development.ini restart
+    kill -1 1
 
 elif [ "${1}" == "update-ckanext" ]; then
     rm -rf /data4dappl &&\
     git clone --depth 1 --single-branch --branch "${2:-master}" https://github.com/OriHoch/data4dappl.git /data4dappl &&\
     cp -rf /data4dappl/ckan/ckanext-odata_org_il/* /ckanext-odata_org_il/ &&\
     rm -rf /data4dappl &&\
-    ckan-pip install -e /ckanext-odata_org_il &&\
+    ../../bin/pip install -e /ckanext-odata_org_il &&\
     /entrypoint.sh server restart
 
 else
