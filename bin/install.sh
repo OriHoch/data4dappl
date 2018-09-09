7bf87030ff871615d1e149fd59b85bd13ec371f4
Ori Hoch <ori@uumpa.com>
2018-09-10 00:54:47 +0300

    upgrade ckan to hasadna-ckan-2.8.1.3

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 pipenv install &&\
-pipenv run pip install -e 'git+https://github.com/hasadna/ckan.git@hasadna-ckan-2.8.1.2#egg=ckan' &&\
+pipenv run pip install -e 'git+https://github.com/hasadna/ckan.git@hasadna-ckan-2.8.1.3#egg=ckan' &&\
 pipenv run pip install -e `pwd`/ckan/ckanext-odata_org_il
 pipenv run pip install -r ckan/requirements-odata.txt
 [ "$?" != "0" ] && echo installation failed && exit 1
--- ckan/entrypoint.sh
+++ ckan/entrypoint.sh
@@ -12,8 +12,11 @@ elif [ "${1}" == "update-ckanext" ]; then
     rm -rf /data4dappl &&\
     git clone --depth 1 --single-branch --branch "${2:-master}" https://github.com/OriHoch/data4dappl.git /data4dappl &&\
     cp -rf /data4dappl/ckan/ckanext-odata_org_il/* /ckanext-odata_org_il/ &&\
+    cp -f /data4dappl/ckan/requirements-odata.txt /ckanext-odata_org_il/ &&\
     rm -rf /data4dappl &&\
+    ../../bin/pip install -e 'git+https://github.com/hasadna/ckan.git@hasadna-ckan-2.8.1.3#egg=ckan' &&\
     ../../bin/pip install -e /ckanext-odata_org_il &&\
+    ../../bin/pip install -r /ckanext-odata_org_il/requirements-odata.txt &&\
     /entrypoint.sh server restart
 
 else
