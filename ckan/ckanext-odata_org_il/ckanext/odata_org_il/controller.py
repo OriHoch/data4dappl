f63cf9bd7e202755e1b8047a9e5b36537bbb3665
Ori Hoch <ori@uumpa.com>
2019-12-17 10:27:22 +0200

    #113 add new tab of "ישויות" in group page

--- ckan/ckanext-odata_org_il/ckanext/odata_org_il/controller.py
+++ ckan/ckanext-odata_org_il/ckanext/odata_org_il/controller.py
@@ -3,9 +3,15 @@ from ckan.controllers.group import GroupController, model, NotAuthorized, abort,
 import json
 
 
+def _related_groups_sorted(context, groups, source_group_name, reversed=False):
+    return list(sorted(_related_groups_iterator(context, groups, source_group_name),
+                       key=lambda g: g['num_related_groups'], reverse=not reversed))
+
+
 def _related_groups_iterator(context, groups, source_group_name):
     for group in groups:
-        if group['name'] == source_group_name: continue
+        if group['name'] == source_group_name:
+            continue
         data_dict = {
             'q': '',
             'fq': 'groups:"%s"' % group['name'],
@@ -14,11 +20,31 @@ def _related_groups_iterator(context, groups, source_group_name):
             'facet.field': ['groups'],
             'rows': 0
         }
+        group_details = get_action('group_show')(context, {
+            'id': group['name'],
+            'include_datasets': False,
+            'include_dataset_count': False,
+            'include_extras': True,
+            'include_users': False,
+            'include_groups': False,
+            'include_tags': False,
+            'include_followers': False
+        })
+        group_extras = {}
+        for extra in group_details['extras']:
+            group_extras[extra['key']] = extra['value']
+        entity_secondary_type = group_extras.get('entity_secondary_type')
+        if not entity_secondary_type:
+            entity_secondary_type = ""
+        entity_secondary_type = unicode(entity_secondary_type).lower().strip()
+        if entity_secondary_type == 'none':
+            entity_secondary_type = ''
         yield {
             'name': group['name'],
             'display_name': group['display_name'],
             'num_datasets': group['count'],
-            'num_related_groups': len(get_action('package_search')(context, data_dict)['search_facets']['groups']['items'])
+            'num_related_groups': len(get_action('package_search')(context, data_dict)['search_facets']['groups']['items']),
+            'entity_secondary_type': entity_secondary_type
         }
 
 
@@ -38,10 +64,7 @@ class GroupEntitiesController(GroupController):
                    'for_view': True, 'extras_as_string': True,
                    'return_query': True}
         q = ''
-        if c.group_dict.get('is_organization'):
-            fq = 'owner_org:"%s"' % c.group_dict.get('id')
-        else:
-            fq = 'groups:"%s"' % c.group_dict.get('name')
+        fq = 'groups:"%s"' % c.group_dict['name']
         data_dict = {
             'q': q,
             'fq': fq,
@@ -51,16 +74,19 @@ class GroupEntitiesController(GroupController):
             'rows': 0
         }
         query = get_action('package_search')(context, data_dict)
-        c.entities_extras = []
-        c.query = query
-        for item in query['search_facets']['groups']['items']:
-            c.entities_extras.append({'key': item['display_name'], 'value': item['count']})
-
         return render('group/entities.html',
-                      extra_vars={'group_type': group_type, 'json': json})
+                      extra_vars={
+                          'group_type': group_type,
+                          'json': json,
+                          'related_groups': _related_groups_sorted(
+                              context,
+                              query['search_facets']['groups']['items'], c.group_dict['name']
+                          )
+                      })
 
     def show_entities_api(self):
         id = request.params.get('name', '')
+        reversed = request.params.get('reversed', '')
         _ = self._ensure_controller_matches_group_type(id)
         context = {'model': model, 'session': model.Session,
                    'user': c.user}
@@ -73,10 +99,7 @@ class GroupEntitiesController(GroupController):
                    'for_view': True, 'extras_as_string': True,
                    'return_query': True}
         q = ''
-        if c.group_dict.get('is_organization'):
-            fq = 'owner_org:"%s"' % c.group_dict.get('id')
-        else:
-            fq = 'groups:"%s"' % c.group_dict.get('name')
+        fq = 'groups:"%s"' % c.group_dict['name']
         data_dict = {
             'q': q,
             'fq': fq,
@@ -99,7 +122,7 @@ class GroupEntitiesController(GroupController):
                 group[k] = v
         return json.dumps({
             'group': group,
-            'related_groups': list(_related_groups_iterator(context, query['search_facets']['groups']['items'], group['name']))
+            'related_groups': _related_groups_sorted(context, query['search_facets']['groups']['items'], group['name'], reversed=reversed == 'true')
         }, ensure_ascii=False, indent=2)
 
     def _get_group_dict(self, id, include_datasets=False):
--- ckan/ckanext-odata_org_il/ckanext/odata_org_il/fanstatic/odata-entities.js
+++ ckan/ckanext-odata_org_il/ckanext/odata_org_il/fanstatic/odata-entities.js
@@ -1,50 +1,47 @@
-$(function() {
-  var onClickRelatedGroup = function($tr) {
-    if (!$tr.find('a[data-expanded]').data('expanded')) {
-      $tr.find('a[data-expanded]').data('expanded', 'true');
-      $tr.find('a[data-expanded]').text('Loading...');
-      $.getJSON('/api/3/action/package_search?fq=groups:"'+$tr.data('relatedgroupname')+'"&facet.field=["groups"]&include_private=false&facet.limit=-1&rows=0').then(function(related_query){
-          var _table = document.getElementById("related_entities_table");
-          $.each(related_query.result.search_facets.groups.items, function(_, related_group) {
-              if (
-                  related_group.name != $(_table).data('originalgroupname')
-                  && related_group.name != $tr.data('relatedgroupname')
-                  && $('#related_entities_table tr[data-relatedgroupname='+related_group.name+']').length == 0
-              ) {
-                var _row = _table.insertRow(-1);
-                $(_row).data('relatedgroupname', related_group.name);
-                var _sourceCell = _row.insertCell(0);
-                var _relatedCell = _row.insertCell(1);
-                _sourceCell.innerHTML = $tr.data('relatedgroupdisplayname');
-                _relatedCell.innerHTML = '<a href="/group/entities?id='+related_group.name+'">'+related_group.display_name + '</a> (' + related_group.count + ' '+$(_table).data('documentslabel')+') <a href="javascript:void(0)" data-expanded="false">'+$(_table).data('expandlabel')+'</a>';
-                $(_row).find('a[data-expanded]').on('click', function() {
-                    onClickRelatedGroup($(_row));
-                });
-              }
-          });
-          $tr.find('a[data-expanded]').remove();
-      });
-    }
-  };
-  $('#related_entities_table tr[data-relatedgroupname]').each(function(_, tr){
-    $(tr).find('a[data-expanded]').on('click', function() {
-      onClickRelatedGroup($(tr));
+$(function () {
+    var _allRelatedGroupNames = [];
+    $('tr[data-relatedgroupname]').each(function(_, tr) {
+        var rgn = $(tr).data('relatedgroupname');
+        if (rgn) {
+            _allRelatedGroupNames.push(''+rgn);
+        }
     });
-  });
+    _allRelatedGroupNames.push(''+$('#related_entities_table').data('originalgroupname'));
+    var onClickRelatedGroup = function ($tr) {
+        var $a = $tr.find('a[data-expanded]');
+        if (!$a.data('expanded')) {
+            $a.data('expanded', 'true');
+            $a.text('Loading...');
+            $.getJSON('/group/entities/api?name=' + $tr.data('relatedgroupname') + '&reversed=true').then(function (apires) {
+                var _table = document.getElementById("related_entities_table");
+                var rowIndex = $tr[0].rowIndex+1;
+                $.each(apires.related_groups, function (_, related_group) {
+                    if (_allRelatedGroupNames.indexOf(''+related_group.name) == -1) {
+                        _allRelatedGroupNames.push(''+related_group.name);
+                        var _row = _table.insertRow(rowIndex);
+                        $(_row).data('relatedgroupname', related_group.name);
+                        var _sourceCell = _row.insertCell(0);
+                        var _relatedCell = _row.insertCell(1);
+                        var _relatedTypeCell = _row.insertCell(2);
+                        var _numRelatedEntitiesCell = _row.insertCell(3);
+                        var _numRelatedDocumentsCell = _row.insertCell(4);
+                        _sourceCell.innerHTML = apires.group.display_name;
+                        _relatedCell.innerHTML = '<a href="/group/entities?name=' + related_group.name + '">' + related_group.display_name + '</a>';
+                        _relatedTypeCell.innerHTML = '' + related_group.entity_secondary_type;
+                        _numRelatedEntitiesCell.innerHTML = '' + related_group.num_related_groups + ' <a href="javascript:void(0);" data-expanded="false">('+ $(_table).data('expandlabel') +')</a>';
+                        _numRelatedDocumentsCell.innerHTML = '' + related_group.num_datasets;
+                        $(_row).find('a[data-expanded]').on('click', function () {
+                            onClickRelatedGroup($(_row));
+                        });
+                    }
+                });
+                $a.remove();
+            });
+        }
+    };
+    $('#related_entities_table tr[data-relatedgroupname]').each(function (_, tr) {
+        $(tr).find('a[data-expanded]').on('click', function () {
+            onClickRelatedGroup($(tr));
+        });
+    });
 });
-
-
-// function expand_related_groups(source_group_id, source_group_display_name) {
-//       $.getJSON('/api/3/action/package_search?fq=groups:"'+source_group_id+'"&facet.field=["groups"]&include_private=false&facet.limit=-1&rows=0').then(function(related_query){
-//         $.each(related_query.result.search_facets.groups.items, function(_, related_group) {
-//           if (related_group.name != original_group_id && related_group.name != source_group_id) {
-//             var _table = document.getElementById("related_entities_table");
-//             var _row = _table.insertRow(-1);
-//             var _sourceCell = _row.insertCell(0);
-//             var _relatedCell = _row.insertCell(1);
-//             _sourceCell.innerHTML = source_group_display_name;
-//             _relatedCell.innerHTML = related_group.display_name + ' (' + related_group.count + ' {{ _('Documents') }}) <a href="expand_related_groups()"></a>';
-//           }
-//         });
-//       })
-//     }
